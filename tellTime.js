c8a8ad48b59da7675c286d69f2ff6c2159d97033
loyti <brice@loyti.com>
2017-06-26 11:09:57 -0700

    updated html & css files

--- tellTime.js
+++ tellTime.js
@@ -1,34 +1,34 @@
-var HOUR = [];
-var MINUTE = []];
-var PERIOD = ["AM", "PM"];
-var morning = boolean;
-var evening = boolean;
+var HOUR = 8; 
+var MINUTE = 50;
+var PERIOD = "AM";
 var messageM = "<p>It's almost 9 in the moring</p>";
 var messageE = "<p>It's just after 7 in the evening</p>";
-var tickTock = [HOUR, MINUTE, PERIOD];
-
+var morning;
+var evening;
 function morningTime(){
+    morning = true;
+    evening = false;
     if (morning == true){
         HOUR = 8;
         MINUTE = 50;
         PERIOD = "AM";
     }
-    return document.write(messageM + tickTock);
+    var tickTock = [HOUR, MINUTE, PERIOD];
+    return document.write(messageM + "<p>" + tickTock + "</p>");
 };
 function eveningTime(){
+    evening = true; 
+    morning = false;
     if (evening == true){
         HOUR = 7;
         MINUTE = 15;
         PERIOD = "PM";
     }
-    return document.write(messageE + tickTock);
+    var tickTock = [HOUR, MINUTE, PERIOD];
+    return document.write(messageE + "<p>" + tickTock + "</p>");
 };
 
 function tellTime (){
-    morning = true;
-    for( var i = 0; i < 2; i++){
-        morningTime();
-    };
-    
+    morningTime();
 }
 tellTime();
